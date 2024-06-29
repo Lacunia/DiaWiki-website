bb3421cc56c3f1c0ac5eafb87217530a8aeec590
Lucy Lin <lucylin6699@gmail.com>
2024-06-29 17:42:03 -0400

    added login/register function

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # DiaWiki-website
 This is my personal project - a website dedicated to diabetes patients.
 
+The version made with Wix: https://lucylin6699.wixsite.com/diawiki
+
 As my family on my mother's side has the genes for diabetes, I myself am subjected to risks of getting diabetes too. I have witnessed the pain and suffering my grandmother went through during her last days as a diabetes patients, as well as my mother's effort to prevent herself from following the same path. As such, I thought that perhaps building a website that is dedicated to providing information to diabetes patients (or potential patients) and allowing them to connect with one another is a great way to contribute in this community with my own effort. 
+
+Previously, I used the website builder to create it while designing the UI and collecting the content by myself. Currently I'm working on using Python Flask, SQLAlchemy, HTML, CSS, and Javascript to recreate this website. I want to be able to recreate it now that I have certain capabilities of doing so by myself!
--- app.py
+++ app.py
@@ -12,6 +12,20 @@ app.config['SECRET_KEY'] = '91f6d6f8e8d9c5f91efb94922894b60a'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diawiki.db' # this set up the path to our database
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 60) # keep the session on for 60 min
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+# databases
+class Person(db.Model):
+    __tablename__ = 'Person'
+    username = db.Column(db.String(20), primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable = False)
+
+    def __repr__(self):
+        return f"Person('{self.username}', '{self.email}')"
+
+with app.app_context(): # create any database that are new
+    db.create_all()
 
 
 @app.route('/') # the function will be executed if the web route has '/' or '/home' as extensions
@@ -61,13 +75,77 @@ def treatmentG():
 def prevention():
     return render_template('prevention.html')
 
+
 @app.route('/forum')
 def forum():
-    return render_template('forum.html')
+    if "user" in session:
+        return render_template('forum.html')
+    else:
+        return render_template('warning.html')
+
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    if "user" in session:
+        return render_template('profile.html')
+    else:
+        return render_template('warning.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    else:
+        username=request.form['username']
+        password=request.form['password']
+        person = Person.query.filter_by(username=username).first()
+        if not person or not bcrypt.check_password_hash(person.password, password):
+            flash('Please check your login details and try again', 'error') #'error' is optional
+            return render_template('login.html')
+        else:
+            session['user'] = username
+            session.permanent=True  # closing the browser will not log out
+            flash('Logged in Successfully!')
+            return redirect(url_for('profile'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        username=request.form['username']
+        email=request.form['email']
+        # for added security when using bcrypt
+        hashed_password=bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+
+        # check if this username is already taken
+        person = Person.query.filter_by(username=username).first()
+        if person:
+            flash('This username already exist! Please choose another one!')
+            return render_template('register.html')
+        else:
+            reg_details = (
+                username,
+                email,
+                hashed_password,
+            )
+            add_users(reg_details)
+            flash('Registration successful! Please login now.')
+            return redirect(url_for('login')) # redirect from the current page to this page
+
+# add the register user into our db
+def add_users(reg_details):
+    user = Person(username=reg_details[0], email=reg_details[1], password=reg_details[2])
+    db.session.add(user)
+    db.session.commit()
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
